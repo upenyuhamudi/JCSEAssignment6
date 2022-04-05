d85dc8039cd19931f52d206b938e9fb2a90914b4
upenyuhamudi <uhamudikuwanda@gmail.com>
2022-04-05 15:00:40 +0200

    Logging and Unit Testing Commit

--- test_testGrades.py
+++ test_testGrades.py
@@ -1,10 +1,16 @@
 import unittest
 import testGrades
 
+
 class TestGradesCalc(unittest.TestCase):
 
+#Check if the number of students are correct
     def test_numberofstudents(self):
-        self.assertEqual(11,len(testGrades.sql_reader))
+        self.assertEqual(10,len(testGrades.sql_reader))
+#Check if grades match calculated average marks
+    def test_averageMarkGrades(self):
+            self.assertEqual("A",testGrades.val[0][3])
 
 if __name__ == '__main__':
     unittest.main()
+
